11ca8bf90d274e4296d32a0307211e9f5f9f8ac2
Tran Van Tien <tvtienk2@gmail.com>
2020-04-05 10:27:49 +0700

    up

--- application/views/coronavirus_vw.php
+++ application/views/coronavirus_vw.php
@@ -1,73 +1,6 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-    <meta name="description" content="Corona Virus Tracker by Furkan KAHVECİ">
-    <meta name="keywords" content="Corona, COVID-19, Coronavirus, Coronavirus Tracker">
-    <meta name="author" content="Furkan KAHVECİ">
-    <meta http-equiv="X-UA-CompatibBle" content="IE=edge"/>
-
-    <title>Thống kê Covid-19 | tvtien2k</title>
-    <link rel="shortcut icon" href="<?php echo base_url(); ?>assets/images/favicon.ico">
-
-    <link href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-    <link href="<?php echo base_url(); ?>assets/css/icons.css" rel="stylesheet" type="text/css">
-    <link href="<?php echo base_url(); ?>assets/css/style.css" rel="stylesheet" type="text/css">
-    <link href="<?php echo base_url(); ?>assets/plugins/datatables/jquery.dataTables.min.css" rel="stylesheet"
-          type="text/css"/>
-    <link href="<?php echo base_url(); ?>assets/plugins/datatables/buttons.bootstrap4.min.css" rel="stylesheet"
-          type="text/css"/>
-    <link href="<?php echo base_url(); ?>assets/plugins/datatables/fixedHeader.bootstrap.min.css" rel="stylesheet"
-          type="text/css"/>
-    <link href="<?php echo base_url(); ?>assets/plugins/datatables/responsive.bootstrap.min.css" rel="stylesheet"
-          type="text/css"/>
-    <link href="<?php echo base_url(); ?>assets/plugins/datatables/dataTables.bootstrap4.min.css" rel="stylesheet"
-          type="text/css"/>
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-
-    <script type="text/javascript">
-        google.charts.load('current', {
-            'packages': ['geochart'],
-            // Note: you will need to get a mapsApiKey for your project.
-            // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
-            'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
-        });
-        google.charts.setOnLoadCallback(drawRegionsMap);
-
-        function drawRegionsMap() {
-            var data = google.visualization.arrayToDataTable([
-                ['Country', 'Confirmed Cases',], <?php foreach ($RESULT_DATA as $map) {
-                echo '["' . $map['country'] .
-                    '",' . $map['cases'] .
-                    '],';
-            } ?>
-            ]);
-
-            var options = {
-                colorAxis: {
-                    colors: ['#D2E3FC', '#8AB4F8', '#4285F4', '#1967D2', '#174EA6',]
-                },
-                backgroundColor: '#2A323C',
-                legend: {
-                    textStyle: {
-                        color: '#000',
-                        fontSize: 14
-                    }
-                }
-            };
-
-            var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
-
-            chart.draw(data, options);
-        }
-    </script>
-
-</head>
-
-
+<?php $this->load->view("template/head_vw"); ?>
+<?php $this->load->view("geochart_vw"); ?>
 <body>
-
 <div id="content-page">
     <div class="content">
         <div class="row ">
@@ -76,7 +9,6 @@
                     <h4 class="page-title" style="text-align:center;">Số liệu thống kê trên toàn thế giới</h4>
                 </div>
             </div>
-
         </div>
         <div class="page-content-wrapper ">
             <div class="container-fluid">
@@ -123,7 +55,7 @@
                     <div class="col-sm-6 col-lg-3">
                         <div class="card text-center">
                             <div class="card-heading">
-                                <h4 class="card-title text-muted mb-0">Hôm qua</h4>
+                                <h4 class="card-title text-muted mb-0">Mới</h4>
                             </div>
                             <div class="card-body p-t-10">
                                 <h2 class="m-t-0 m-b-15">
@@ -135,8 +67,7 @@
                 </div>
                 <h4 class="page-title" style="text-align:center;">Số liệu thống kê tại Việt Nam</h4>
                 <?php foreach ($RESULT_DATA as $result) {
-                    if ($result['country'] == "Vietnam") {
-                        ?>
+                    if ($result['country'] == "Vietnam") { ?>
                         <div class="row">
                             <div class="col-sm-6 col-lg-3">
                                 <div class="card text-center">
@@ -190,8 +121,10 @@
                                 </div>
                             </div>
                         </div>
-                    <?php }
+                        <?php
+                    }
                 } ?>
+
 
                 <div class="row">
                     <div class="col-sm-12 col-lg-12">
@@ -203,7 +136,7 @@
                     <div class="col-lg-12">
                         <div class="card">
                             <div class="card-body">
-                                <h4 class="m-b-30 m-t-0">Số liệu thống kê</h4>
+                                <h4 class="m-b-30 m-t-0">Statistics</h4>
                                 <div class="row">
                                     <div class="col-12">
                                         <div class="table-responsive">
@@ -214,9 +147,9 @@
                                                 <tr>
                                                     <th>Quốc gia / Vùng lãnh thổ</th>
                                                     <th>Trường hợp</th>
-                                                    <th>Trường hợp / triệu người</th>
                                                     <th>Tử vong</th>
                                                     <th>Hồi phục</th>
+                                                    <th>Đã test</th>
                                                     <th>Mới</th>
                                                 </tr>
                                                 </thead>
@@ -224,13 +157,17 @@
                                                 <?php foreach ($RESULT_DATA as $result) { ?>
                                                     <tr>
                                                         <td><img
-                                                                src="https://www.gstatic.com/onebox/sports/logos/flags/<?php echo strtolower(str_replace(" ", "_", $result['image'])); ?>_icon_square.svg"
-                                                                height="20"
-                                                                width="20"> <?php echo $result['country']; ?></td>
+                                                                src="<?php if (strpos($result['image'], 'gstatic') !== false) {
+                                                                    echo $result['image'];
+                                                                } else {
+                                                                    echo "https://www.gstatic.com/onebox/sports/logos/flags/" . strtolower(str_replace(" ", "_", $result['image'])) . "_icon_square.svg";
+                                                                } ?>
+                                                            " height="20" width="20"> <?php echo $result['country']; ?>
+                                                        </td>
                                                         <td><?php echo number_format($result['cases']); ?></td>
-                                                        <td><?php echo number_format($result['casesPerOneMillion']); ?></td>
                                                         <td><?php echo number_format($result['deaths']); ?></td>
                                                         <td><?php echo number_format($result['recovered']); ?></td>
+                                                        <td><?php echo number_format($result['totalTests']); ?></td>
                                                         <td><?php echo number_format($result['todayCases']); ?></td>
                                                     </tr>
                                                 <?php } ?>
@@ -241,48 +178,4 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="row ">
-                            <div class=" col-lg-12">
-                                <div class="page-title footerchange">
-                                    © 2020 Coronavirus Tracker - Furkan KAHVECİ - Design by tvtien2k
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-
-            </div>
-
-        </div>
-    </div>
-
-
-</div>
-</div>
-
-<script src="<?php echo base_url(); ?>assets/js/jquery.min.js"></script>
-<script src="<?php echo base_url(); ?>assets/js/popper.min.js"></script>
-<script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
-
-<!-- Required datatable js-->
-<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.js">
-</script>
-<script src="<?php echo base_url(); ?>assets/plugins/datatables/dataTables.bootstrap4.min.js"></script>
-
-<!-- Responsive examples -->
-<script src="<?php echo base_url(); ?>assets/plugins/datatables/dataTables.responsive.min.js"></script>
-<script src="<?php echo base_url(); ?>assets/plugins/datatables/responsive.bootstrap4.min.js"></script>
-
-<script>
-    $(document).ready(function () {
-        $('table').DataTable({
-            order: [],
-            paging: true,
-            "pageLength": 25,
-        });
-    });
-</script>
-</body>
-
-</html>
+                        <?php $this->load->view("template/footer_vw"); ?>
---
+++ application/views/template/head_vw.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="description" content="Corona Virus Tracker by Furkan KAHVECİ Design by tvtien2k">
+    <meta name="keywords" content="Corona, COVID-19, Coronavirus, Coronavirus Tracker">
+    <meta name="author" content="Furkan KAHVECİ">
+    <meta name="Design" content="tvtien2k">
+    <meta http-equiv="X-UA-CompatibBle" content="IE=edge" />
+    <title>Thống kê Covid-19 | tvtien2k</title>
+    <link rel="shortcut icon" href="<?php echo base_url();?>assets/images/favicon.ico">
+    <?php $this->load->view('template/css_vw');?>
+    
